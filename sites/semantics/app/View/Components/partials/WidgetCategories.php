fdc640d4edc39e2e67209a17095bf7ba1b0b120a
rlozach <romain.lozach@infopro-digital.com>
2022-01-07 23:42:19 +0100

    Blog

--- sites/semantics/app/View/Components/partials/WidgetCategories.php
+++ sites/semantics/app/View/Components/partials/WidgetCategories.php
@@ -4,7 +4,7 @@ namespace App\View\Components;
 
 use Illuminate\View\Component;
 
-class partials.widget-categories extends Component
+class WidgetCategories extends Component
 {
     /**
      * Create a new component instance.
